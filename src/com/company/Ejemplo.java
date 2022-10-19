9224344c6ef9432e34cc9cf10bcbe49db45d056a
Alejandro Toledo <altoledo@pichincha.com>
2022-10-19 16:55:36 -0500

    agregar metodo de prueba e imprimir tipos de datos vistos

---
+++ src/com/company/Ejemplo.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Ejemplo {
+
+    public static void main(String[] args) {
+
+        // enteros
+        byte number1  = 1; // 1 byte
+        short number2 = 2; // 2 byte
+        int number3 = 3; // 4 byte
+        long number4 = 4; // 8 byte
+
+        // punto flotante
+        float decimal1 = 5.4f;
+        double decimal2 = 3.76;
+
+        // caracter
+        char caracter1 = 'a';
+
+        // boleano
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // cadenas de texto
+        String nombre = "Alex";
+        String apellido = "Toledo";
+
+        // tipos envoltorios
+        Integer numero1 = null;
+        Long numero2 = 4L;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(caracter1);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(numero1);
+        System.out.println(numero2);
+    }
+}
